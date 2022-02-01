94b23fa0e036d3c42b5a230cf53acf89f786da2f
Dembuxxxx <dems75018@hotmail.fr>
2022-02-01 15:58:03 +0100

    ajout des éléments du cv sur app.js

--- src/App.js
+++ src/App.js
@@ -4,6 +4,20 @@ function App() {
   return <div className='App'>
     {/*  react, materiel ui, react-html2pdf  */}
     {/*  varaibles css */}
+    {/*  colonnes de gauche */}
+
+    {/*  détails personnels - User */}
+    {/*  Compétences - skills */}
+    {/*  langues - skills */}
+    {/*  centres d'intérêts - interests */}
+
+    {/*  colonne de droite*/}
+    {/*  profil - profil */}
+    {/*  formations - formations */}
+    {/*  exp - experiences */}
+
+    {/*  light/dark mode */}
+    {/*  generation pdf */}
 
   </div>    
 }
